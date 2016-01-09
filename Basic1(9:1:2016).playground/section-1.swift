1944ba1dadea7233099d357cabbb1075119815e7
Daxeel Soni <daxeel_soni4444@yahoo.com>
2016-01-09 22:16:03 +0530

    Added Basic1(9/1/2016).playground

---
+++ Basic1(9:1:2016).playground/section-1.swift
@@ -0,0 +1,43 @@
+// Swift Basic 1
+// Date : 9/1/2016
+
+import UIKit
+
+// Mathematical operations
+5 + 4
+10/7.0
+
+// Variables
+var str: String = "Hello, playground"
+var age: Int = 20
+var pi: Float = 2.56
+var bl: Bool = false
+
+age = 21 // Changing variable's value
+
+println(age) // printing variable
+
+let con: Int = 5 // constant
+
+// Printing variable within statement
+println("My age is \(age)")
+println("My age is \(age-2)")
+
+// Comparison operators >, <, =>, =<, ==, !=
+var aman_age = 35
+var naman_age = 20
+
+aman_age > naman_age
+
+var a = 5
+var b = 5
+var c = 10
+
+(a == b) && ((a+b) == c)
+
+// Conditional statement
+if (a == b) {
+    println("yes")
+} else {
+    println("no")
+}
